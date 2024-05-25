1eecf721e243800c941f7a2d935b02c713b60f7e
Axxell04 <axellajones53@gmail.com>
2024-05-25 07:11:11 -0500

    Migration to svelte

--- main.py
+++ main.py
@@ -1,22 +1,36 @@
-import uvicorn
-from fastapi import FastAPI, Response
+from fastapi import FastAPI, BackgroundTasks
 from fastapi.responses import HTMLResponse, FileResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+import sender_mails
+
+class ContextContact(BaseModel):
+    contact_mail: str
+    message: str
 
 app = FastAPI()
+origins = ["http://192.168.1.14:5173", "http://192.168.1.14:4173"]
+
+app.add_middleware(
+    CORSMiddleware, allow_origins=origins, allow_methods=["*"], allow_headers=["*"]
+)
+
 app.mount("/static", StaticFiles(directory="static"), "static")
 
 @app.get("/")
 async def root():
-    return FileResponse("static/html/inicio.html")
-
-@app.get("/proyectos")
-async def proyectos():
-    return FileResponse("static/html/proyectos.html")
+    return FileResponse("static/dist/index.html")
 
 @app.get("/api/get_projects")
 async def get_projects():
     return FileResponse("projects.json")
+
+@app.post("/api/send_mail")
+async def send_email(context_contact : ContextContact, backgound_task: BackgroundTasks):
+    backgound_task.add_task(sender_mails.send_mail, context_contact.message, context_contact.contact_mail)
+    return {"message": "Mensaje enviado con éxito"}
 
 
 # if __name__ == "__main__":
